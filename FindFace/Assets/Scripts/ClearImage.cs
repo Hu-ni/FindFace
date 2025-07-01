c52e3f5860a852f9c36afe826c99ff03df8546de
BangJu0620 <bju0620@naver.com>
2025-07-01 19:24:50 +0900

    ClearImage.cs 수정

--- FindFace/Assets/Scripts/ClearImage.cs
+++ FindFace/Assets/Scripts/ClearImage.cs
@@ -2,26 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// TODO: 클레스 이름 변경.
-// Class 이름은 기존 클래스와 안 겹치게
-// Unity의 Image와 충돌 가능성 있음!!!
-// 가능하면 파일이름과 동일하게
-public class Image : MonoBehaviour
+
+public class ClearImage : MonoBehaviour
 {
     public SpriteRenderer image1;
     public SpriteRenderer image2;
     public SpriteRenderer image1Back;
     public SpriteRenderer image2Back;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //TODO: 해당 코드 의미 없음. 
-        image1.GetComponent<SpriteRenderer>();
-        image2.GetComponent<SpriteRenderer>();
-        image1Back.GetComponent<SpriteRenderer>();
-        image2Back.GetComponent<SpriteRenderer>();
-    }
 
     public void ChangeImage()
     {
