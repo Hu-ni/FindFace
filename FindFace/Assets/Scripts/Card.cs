36025e96fc3adde414b9eb4475fb02ea69b5c83d
seungeon12 <jsu9486@naver.com>
2025-07-01 16:53:39 +0900

    MainScene

---
+++ FindFace/Assets/Scripts/Card.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public int idx = 0;
+
+    public GameObject front;
+    public GameObject back;
+
+    public Animator anim;
+
+
+    public SpriteRenderer frontimage;
+
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void Setting(int number)
+    {
+        idx = number;
+        frontimage.sprite = Resources.Load<Sprite>($"image{idx}");
+
+    }
+
+     public void OpenCard()
+      {
+        anim.SetBool("isOpen", true);
+        front.SetActive(true);
+        back.SetActive(false);
+       
+        if(GameManager.instance.firstCard == null)
+        {
+            GameManager.instance.firstCard = this;
+        }
+        else
+        {
+            GameManager.instance.secondCard = this;
+            GameManager.instance.Matched();
+        }
+      }
+
+    public void DestoeyCard()
+    {
+        Invoke("DestroyCardInvoke", 1.0f);
+    }
+
+
+    void DestroyCardInvoke()
+    {
+        Destroy(gameObject);
+    }
+    public void CloseCard()
+    {
+        Invoke("CloseCardInvoke", 1.0f);
+    }
+
+    void CloseCardInvoke()
+    {
+        anim.SetBool("isOpen", false);
+        front.SetActive(false);
+        back.SetActive(true);
+    }
+    
+}
---
+++ FindFace/Assets/Scripts/GameManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+
+    public Card firstCard;
+    public Card secondCard;
+
+    public Text timeTxt;
+    public GameObject endTxt;
+    public GameObject clearTxt;
+
+    public int cardCount = 0;
+    float time = 90.0f;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        timeTxt.text=time.ToString("N2");
+        if(time <=  0.01f)
+        {
+            endTxt.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+    }
+
+    public void Matched()
+    {
+        if(firstCard.idx == secondCard.idx)
+        {
+            firstCard.DestoeyCard();
+            secondCard.DestoeyCard();
+            cardCount -= 2;
+            if(cardCount == 0)
+            {
+                clearTxt.SetActive(true);
+                Time.timeScale = 0.0f;
+                
+            }
+        }
+        else
+        {
+            firstCard.CloseCard();
+            secondCard.CloseCard();
+        }
+
+        firstCard = null;
+        secondCard = null;
+    }
+}
