9da590b7b58d9f0d4b5752f27ec77e8d01de1450
ntw0926 <1567wkddnjs@gmail.com>
2025-07-02 14:30:39 +0900

    Ranking Size Based on Rank
    + Added : Higher Rank now takes Higher size
    + Added : 1st, 2nd, and 3rd now changes ranking color.

--- FindFace/Assets/Scripts/RankingBoard.cs
+++ FindFace/Assets/Scripts/RankingBoard.cs
@@ -17,6 +17,68 @@ public class RankingBoard : MonoBehaviour
                 Console.WriteLine(i.ToString());
                 GameObject panel = Instantiate(panelPrefab, this.transform);
                 SetPanel(PlayerPrefs.GetFloat(RankingManager.Instance.GenKey(i)), i + 1, panel.transform);
+
+                RectTransform rt = panel.GetComponent<RectTransform>();
+                Text[] texts = panel.transform.GetComponentsInChildren<Text>();
+                Outline[] outlines = panel.transform.GetComponentsInChildren<Outline>();
+                if (i == 0)
+                {
+                    rt.sizeDelta = new Vector2(rt.sizeDelta.x, 150);
+                    foreach (Text text in texts)
+                    {
+                        text.color = new Color(211 / 255.0f, 175 / 255.0f, 55 / 255.0f);
+                        text.fontSize = 120;
+                    }
+                    foreach (Outline outline in outlines)
+                    {
+                        outline.effectColor = Color.yellow;
+                        outline.effectDistance = new Vector2(3, -3);
+                    }
+                }
+                else if (i == 1)
+                {
+                    rt.sizeDelta = new Vector2(rt.sizeDelta.x, 120);
+                    foreach (Text text in texts)
+                    {
+                        text.color = new Color(196 / 255.0f, 196 / 255.0f, 196 / 255.0f);
+                        text.fontSize = 80;
+                    }
+
+                    foreach (Outline outline in outlines)
+                    {
+                        outline.effectColor = Color.gray;
+                        outline.effectDistance = new Vector2(2, -2);
+                    }
+                }
+                else if (i == 2)
+                {
+                    rt.sizeDelta = new Vector2(rt.sizeDelta.x, 100);
+                    foreach (Text text in texts)
+                    {
+                        text.color = new Color(206 / 255.0f, 137 / 255.0f, 70 / 255.0f);
+                        text.fontSize = 60;
+                    }
+
+                    foreach (Outline outline in outlines)
+                    {
+                        outline.effectColor = new Color(255/255.0f, 165/255.0f, 0);
+                        outline.effectDistance = new Vector2(1.5f, -1.5f);
+                    }
+                }
+                else
+                {
+                    rt.sizeDelta = new Vector2(rt.sizeDelta.x, 80);
+                    foreach (Text text in texts)
+                    {
+                        text.fontSize = 40;
+                    }
+                    foreach (Outline outline in outlines)
+                    {
+                        outline.effectColor = Color.white;
+                        outline.effectDistance = new Vector2(0.2f, 0.2f);
+                    }
+                }
+
             }
             else
                 break;
