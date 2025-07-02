97af354286013136d4334efb345c09ef7e0ecf0f
kys2134 <kys21244@naver.com>
2025-07-02 15:18:38 +0900

    버튼 이미지

--- FindFace/Assets/Scripts/StartButton.cs
+++ FindFace/Assets/Scripts/StartButton.cs
@@ -1,11 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 public class StartButton : MonoBehaviour
 {
-    public void StartScene()
+    private bool isRotating = false;
+
+    public void OnClickStart()
     {
-        SceneManager.LoadScene("StartScene");
+        if (!isRotating)
+            StartCoroutine(RotateAndLoadScene());
+    }
+
+    private IEnumerator RotateAndLoadScene()
+    {
+        isRotating = true;
+
+        float duration = 0.5f;
+        float elapsed = 0f;
+        Quaternion startRotation = transform.rotation;
+        Quaternion endRotation = Quaternion.Euler(180, 0, 0); 
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            transform.rotation = Quaternion.Lerp(startRotation, endRotation, elapsed / duration);
+            yield return null;
+        }
+
+        
+        SceneManager.LoadScene("MainScene");
     }
 }
