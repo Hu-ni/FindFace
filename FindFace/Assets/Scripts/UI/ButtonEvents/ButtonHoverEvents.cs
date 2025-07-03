670411bcdc4a4870bd63de9f0f871cf7eee28aa7
ParkHun <hundl5789@gmail.com>
2025-07-03 19:34:52 +0900

    버튼 호버 이벤트 통일
    구조 변경 진행중

--- FindFace/Assets/Scripts/UI/ButtonEvents/ButtonHoverEvents.cs
+++ FindFace/Assets/Scripts/UI/ButtonEvents/ButtonHoverEvents.cs
@@ -7,15 +7,27 @@ public class ButtonHoverEvents : MonoBehaviour, IPointerEnterHandler, IPointerEx
     // Assets/Animation/Ui/Button/Button.controller
     public Animator anim;
 
+    [Header("버튼 애니메이션")]
+    public bool isWave;
+    public bool isZoominOut;
+
     public void OnPointerEnter(PointerEventData eventData)
     {
         gameObject.transform.localScale = new Vector3(transform.localScale.x + 0.2f, transform.localScale.y + 0.2f);
-        anim.SetBool("wave", true);
+        if(isWave)
+            anim.SetBool("wave", true);
+
+        if (isZoominOut)
+            anim.SetBool("zoominout", true);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         gameObject.transform.localScale = new Vector3(transform.localScale.x - 0.2f, transform.localScale.y - 0.2f);
-        anim.SetBool("wave", false);
+
+        if(isWave)
+            anim.SetBool("wave", false);
+        if (isZoominOut)
+            anim.SetBool("zoominout", false);
     }
 }
