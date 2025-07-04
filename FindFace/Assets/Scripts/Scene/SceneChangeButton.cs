c268a5e3cf18079aa44707e2358d22431b003896
ParkHun <hundl5789@gmail.com>
2025-07-04 09:15:28 +0900

    코드 정리

---
+++ FindFace/Assets/Scripts/Scene/SceneChangeButton.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[Serializable]
+public enum SceneNames
+{
+    StartScene = 0,
+    MainScene,
+    ClearScene,
+    GameOverScene,
+    RankingScene
+}
+
+// 기존 버튼 대체
+public class SceneChangeButton : MonoBehaviour
+{
+    public SceneNames SceneNames;
+
+    [Header("옵션")]
+    public bool isFadeOutBlack;
+    public bool isFadeOutWhite;
+    public bool isFadeOutPicture;
+
+    [Header("씬 이동 전에 처리할 이벤트")]
+    public UnityEvent BeforeEvents;
+
+    public void OnButtonClick()
+    {
+        BeforeEvents?.Invoke();
+
+        if (isFadeOutBlack)
+            SceneController.Instance.StartFadeOutB(SceneNames.ToString());
+        else if (isFadeOutWhite)
+            SceneController.Instance.StartFadeOutW(SceneNames.ToString());
+        else if(isFadeOutPicture)
+            SceneController.Instance.StartFadeOutPicture(SceneNames.ToString());
+        else 
+            SceneController.Instance.SceneChange(SceneNames.ToString());
+
+    }
+}
---
+++ FindFace/Assets/Scripts/Scene/SceneController.cs
@@ -0,0 +1,134 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+//public static class SceneNames
+//{
+//    public const string ClearScene = "ClearScene";
+//    public const string GameOverScene = "GameOverScene";
+//    public const string MainScene = "MainScene";
+//    public const string RankingScene = "ClearScene";
+//    public const string StartScene = "StartScene";
+//}
+
+public class SceneController : MonoBehaviour
+{
+    public static SceneController Instance { get; private set; }
+
+    public Animator transition;
+    public GameObject maskImage;
+
+    private bool isLoading = false;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        maskImage.SetActive(false);
+    }
+
+    #region 페이드 아웃
+    // 페이드 아웃(검은색)
+    public void StartFadeOutB(string sceneName)
+    {
+        if (isLoading)
+            return;
+        StartCoroutine(FadeOutAndLoad(sceneName, true));
+    }
+
+    // 페이드 아웃(흰색)
+    public void StartFadeOutW(string sceneName)
+    {
+        if (isLoading)
+            return;
+        StartCoroutine(FadeOutAndLoad(sceneName, false));
+    }
+
+
+    IEnumerator FadeOutAndLoad(string sceneName, bool isBlack)
+    {
+        if(isLoading)
+            yield break;
+
+        isLoading = true;
+        if (Time.timeScale <= 0f)
+            Time.timeScale = 1f;
+
+        if (isBlack)
+            transition.SetTrigger("FadeOutB");
+        else
+            transition.SetTrigger("FadeOutW");
+        yield return new WaitForSeconds(1.0f); // Fade 애니메이션 길이
+        yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+
+        isLoading = false;
+    }
+
+    public void StartFadeOutPicture(string sceneName)
+    {
+        if (isLoading)
+            return;
+
+        int idx = Random.Range(0, 10);
+        maskImage.SetActive(true);
+        maskImage.GetComponent<Image>().sprite = Resources.Load<Sprite>($"image{idx}");
+
+        StartCoroutine(FadeOutWithPicture(sceneName));
+        
+    }
+
+    IEnumerator FadeOutWithPicture(string sceneName)
+    {
+        if (isLoading)
+            yield break;
+
+        isLoading = true;
+        if (Time.timeScale <= 0f)
+            Time.timeScale = 1f;
+
+        transition.SetTrigger("FadeOutPicture");
+        yield return new WaitForSeconds(1.0f); // Fade 애니메이션 길이
+        yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+
+        maskImage.SetActive(false);
+        isLoading = false;
+    }
+    #endregion
+
+    // 귀찮으므로 추후 연결해보고 고민해보기로 
+    //#region 슬라이드아웃
+    //public void SlideOut(string sceneName)
+    //{
+    //    StartCoroutine(SlideOutAndLoad(sceneName));
+    //}
+
+    //IEnumerator SlideOutAndLoad(string sceneName)
+    //{
+
+    //    yield return new WaitForSeconds(1.0f);
+    //    SceneManager.LoadScene(sceneName);
+    //}
+    //#endregion
+    public void SceneChange(string sceneName)
+    {
+        if (isLoading)
+            return;
+        isLoading = true;
+        SceneManager.LoadScene(sceneName);
+        isLoading = false;
+    }
+}
